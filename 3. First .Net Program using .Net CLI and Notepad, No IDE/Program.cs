66003a97fe3ea7afa0f4d839a3ccc3e5c8ee6860
SUSHANT\susha <sushant_patil@outlook.com>
2025-06-26 19:21:45 +0530

    no message

--- 3. First .Net Program using .Net CLI and Notepad, No IDE/Program.cs
+++ 3. First .Net Program using .Net CLI and Notepad, No IDE/Program.cs
@@ -6,6 +6,21 @@ namespace MyFirstProgram
 	{
 		public static void Main()
 		{
+			string fullName = "james bond";
+			int age = 10;
+			
+			int number1;
+			int number2;
+			int result;
+			
+			number1 = 10;
+			number2 = 20;
+			
+			result = number1 + number2;
+			
+			Console.WriteLine("my name is" + fullName);
+			Console.WriteLine("age is "+ age);
+			Console.WriteLine("the result is "+ result);
 			Console.WriteLine("Hello World!");
 		}
 	}
